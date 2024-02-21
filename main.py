1ae6c12d176b185f153f32c9c50d0a3c1a8142b7
xammmue <zwackelmann98@gmail.com>
2024-02-21 23:02:30 +0100

    Add endless loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ServerQuery import ServerQuery
 import configparser
 import paho.mqtt.publish as publish
+from time import sleep
 
 config = configparser.ConfigParser()
 
@@ -10,19 +11,21 @@ connection_details = config['connection']
 server_details = config['vserver']
 mqtt_details = config['mqtt']
 
-query = ServerQuery(host=connection_details['host'], port=int(connection_details['port']),
-                    username=connection_details['user'], password=connection_details['password'],
-                    server_id=int(server_details['server_id']))
+while True:
+    query = ServerQuery(host=connection_details['host'], port=int(connection_details['port']),
+                        username=connection_details['user'], password=connection_details['password'],
+                        server_id=int(server_details['server_id']))
 
-host_info = query.load_host_info()
-print(host_info.virtual_servers_total_clients_online)
+    host_info = query.load_host_info()
+    print(host_info.virtual_servers_total_clients_online)
 
-# client_list = query.load_client_list()
-# for client in client_list:
-#     print(client)
+    # client_list = query.load_client_list()
+    # for client in client_list:
+    #     print(client)
 
-print('There are {}/{} users online'.format(host_info.virtual_servers_total_clients_online, host_info.virtual_servers_total_max_clients))
+    print('There are {}/{} users online'.format(host_info.virtual_servers_total_clients_online, host_info.virtual_servers_total_max_clients))
 
-publish.single(mqtt_details['topic'], host_info.virtual_servers_total_clients_online, hostname=mqtt_details['host'],
-               port=int(mqtt_details['port']),
-               auth={'username': mqtt_details['username'], 'password': mqtt_details['password']})
+    publish.single(mqtt_details['topic'], host_info.virtual_servers_total_clients_online, hostname=mqtt_details['host'],
+                   port=int(mqtt_details['port']),
+                   auth={'username': mqtt_details['username'], 'password': mqtt_details['password']})
+    sleep(10)
