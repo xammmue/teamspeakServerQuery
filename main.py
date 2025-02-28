e88f2ec60f24bb60b72ebb04e7fccbb84af24b2c
xammmue <zwackelmann98@gmail.com>
2025-02-28 15:33:29 +0100

    Switch to continuous running in docker with a 30s delay

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from time import sleep
 
 config = configparser.ConfigParser()
 
-config.read('/home/pi/teamspeakServerQuery/config.cfg')
+config.read('config.cfg')
 
 connection_details = config['connection']
 server_details = config['vserver']
 mqtt_details = config['mqtt']
 
-query = ServerQuery(host=connection_details['host'], port=int(connection_details['port']),
-                username=connection_details['user'], password=connection_details['password'],
-                server_id=int(server_details['server_id']))
+query = ServerQuery(
+    host=connection_details['host'],
+    port=int(connection_details['port']),
+    username=connection_details['user'],
+    password=connection_details['password'],
+    server_id=int(server_details['server_id'])
+)
 
-host_info = query.load_host_info()
-print(host_info.virtual_servers_total_clients_online)
+while True:
+    host_info = query.load_host_info()
+    clients_online = host_info.virtual_servers_total_clients_online
+    max_clients = host_info.virtual_servers_total_max_clients
 
-    # client_list = query.load_client_list()
-    # for client in client_list:
-    #     print(client)
+    print(clients_online)
+    print('There are {}/{} users online'.format(clients_online, max_clients))
 
-print('There are {}/{} users online'.format(host_info.virtual_servers_total_clients_online, host_info.virtual_servers_total_max_clients))
+    publish.single(
+        mqtt_details['topic'],
+        clients_online,
+        hostname=mqtt_details['host'],
+        port=int(mqtt_details['port']),
+        auth={'username': mqtt_details['username'], 'password': mqtt_details['password']}
+    )
 
-publish.single(mqtt_details['topic'], host_info.virtual_servers_total_clients_online, hostname=mqtt_details['host'],
-           port=int(mqtt_details['port']),
-           auth={'username': mqtt_details['username'], 'password': mqtt_details['password']})
+    sleep(30)
