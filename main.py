b2c9d0c391c7f93ec74a260b7767ea1287527673
xammmue <zwackelmann98@gmail.com>
2025-02-28 15:01:28 +0100

    Add adaptions from working environment

--- main.py
+++ main.py
@@ -5,27 +5,25 @@ from time import sleep
 
 config = configparser.ConfigParser()
 
-config.read('config.cfg')
+config.read('/home/pi/teamspeakServerQuery/config.cfg')
 
 connection_details = config['connection']
 server_details = config['vserver']
 mqtt_details = config['mqtt']
 
-while True:
-    query = ServerQuery(host=connection_details['host'], port=int(connection_details['port']),
-                        username=connection_details['user'], password=connection_details['password'],
-                        server_id=int(server_details['server_id']))
+query = ServerQuery(host=connection_details['host'], port=int(connection_details['port']),
+                username=connection_details['user'], password=connection_details['password'],
+                server_id=int(server_details['server_id']))
 
-    host_info = query.load_host_info()
-    print(host_info.virtual_servers_total_clients_online)
+host_info = query.load_host_info()
+print(host_info.virtual_servers_total_clients_online)
 
     # client_list = query.load_client_list()
     # for client in client_list:
     #     print(client)
 
-    print('There are {}/{} users online'.format(host_info.virtual_servers_total_clients_online, host_info.virtual_servers_total_max_clients))
+print('There are {}/{} users online'.format(host_info.virtual_servers_total_clients_online, host_info.virtual_servers_total_max_clients))
 
-    publish.single(mqtt_details['topic'], host_info.virtual_servers_total_clients_online, hostname=mqtt_details['host'],
-                   port=int(mqtt_details['port']),
-                   auth={'username': mqtt_details['username'], 'password': mqtt_details['password']})
-    sleep(10)
+publish.single(mqtt_details['topic'], host_info.virtual_servers_total_clients_online, hostname=mqtt_details['host'],
+           port=int(mqtt_details['port']),
+           auth={'username': mqtt_details['username'], 'password': mqtt_details['password']})
